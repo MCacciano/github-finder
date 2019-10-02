a00ca5c64b7c265b27ee725759f59c5a398aa9ce
michael.cacciano@vmlyr.com <michael.cacciano@vmlyr.com>
2019-10-02 11:13:02 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+
+import github from './axios/github';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	};
 
+	async componentDidMount() {
+		this.setState({ loading: true });
+
+		try {
+			const res = await github.get('/users');
+			const users = res.data;
+
+			this.setState({ users, loading: false });
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	render() {
+		const { users, loading } = this.state;
+		return (
+			<div>
+				<Navbar title='Github Finder' icon='fab fa-github' />
+				<div className='container'>
+					<Users users={users} loading={loading} />
+				</div>
+			</div>
+		);
+	}
+}
 export default App;
